73a5c8d85b20df272f836f84c7f25f9c7999fa2a
lucasdias2906 <lucasdias2906@gmail.com>
2020-11-02 12:38:08 -0300

    começando o projeto com java

---
+++ spring-boot-myproject/src/main/java/com/lucasspringboot/springbootmyproject/SpringBootMyprojectApplication.java
@@ -0,0 +1,13 @@
+package com.lucasspringboot.springbootmyproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootMyprojectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootMyprojectApplication.class, args);
+	}
+
+}
---
+++ spring-boot-myproject/src/test/java/com/lucasspringboot/springbootmyproject/SpringBootMyprojectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lucasspringboot.springbootmyproject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootMyprojectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
